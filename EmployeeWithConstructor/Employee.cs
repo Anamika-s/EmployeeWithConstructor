062ffb89c51dbeb6f1f80fd16de7678c8d5e0afd
Administrator <Administrator@WINDOWS-UNR7VLH>
2022-04-07 19:35:11 +0530

    Add project files.

---
+++ EmployeeWithConstructor/Employee.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EmployeeWithConstructor
+{
+    class Employee
+    {
+        int id;
+        string name;
+        int exp;
+        static string hrManager;
+        public const string comapanyName = "Wipro";
+        decimal da, hra, pf, netSalary, basicSalary;
+        public Employee() { }
+        static Employee()
+        {
+            hrManager = "Ajay";
+
+        }
+
+        public Employee(int id, string name)
+        {
+
+            this.id = id;
+            this.name = name;
+            Console.WriteLine("Enter Exp");
+            exp = Byte.Parse(Console.ReadLine());
+            Console.WriteLine("Enter Basic Salary");
+            basicSalary = Convert.ToInt32(Console.ReadLine());
+        }
+        public Employee(int id, string name, decimal basicSalary,
+            int exp)
+        {
+
+            this.id = id;
+            this.name = name;
+            this.basicSalary = basicSalary;
+            this.exp = exp;
+        }
+        public void GetDetails() 
+        {
+            Console.WriteLine("Enter Id");
+            id = Byte.Parse(Console.ReadLine());
+            Console.WriteLine("Enter name");
+            name =  Console.ReadLine();
+            Console.WriteLine("Enter Exp");
+            exp = Byte.Parse(Console.ReadLine());
+            Console.WriteLine("Enter Basic Salary");
+            basicSalary = Convert.ToInt32(Console.ReadLine());
+        }
+
+        public void CalculateSalary()
+        {
+
+        }
+        public void DisplayDetails()
+        {
+        }
+        public static void DisplayManagerName()
+        {
+            Console.WriteLine("Manager Name is  " + hrManager);
+        }
+    }
+}
---
+++ EmployeeWithConstructor/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace EmployeeWithConstructor
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<Employee> list = new List<Employee>();
+            Employee employee5 = new Employee();
+            employee5.GetDetails();
+            list.Add(employee5);
+            Employee employee1 = new Employee(1, "Ajay");
+            list.Add(employee1);
+
+            Employee employee2 = new Employee(1, "Deepak", 12000, 3);
+            list.Add(employee2);
+            // NAMED PARAMETER
+            Employee employee3 = new Employee(name: "Lalit", id: 3, exp: 8, basicSalary: 12000);
+            list.Add(employee3);
+
+            foreach (Employee employee in list)
+            {
+                employee.CalculateSalary();
+                employee.DisplayDetails();
+            }
+            Console.WriteLine("Company Name is "  + Employee.comapanyName);
+            Employee.DisplayManagerName();
+            Console.WriteLine("No of Employees are " + list.Count);
+       }
+    }
+}
